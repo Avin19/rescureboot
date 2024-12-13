459eebd063d62be10f2a91d65a8b658925ab3fe5
Avin19 <avinashy1993@gmail.com>
2024-12-13 11:44:53 +0530

    Working UI Main Manager

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using GoogleMobileAds.Api;
 
 
 
 public class AdsManager : MonoBehaviour
 {
-  
-
+#if UNITY_ANDROID
+    private string _adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#elif UNITY_IPHONE
+  private string _adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+  private string _adUnitId = "unused";
+#endif
+    private void Start()
+    {
+        MobileAds.Initialize(initStatus => { });
+    }
 }
 
 
